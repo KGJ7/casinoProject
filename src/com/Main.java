3da4a70245bff788a641c2cc2fea1abcb0e59df3
KGJ7 <glectechs16@gmail.com>
2021-01-31 21:01:02 +0000

    Initial commit

---
+++ src/com/Main.java
@@ -0,0 +1,26 @@
+package com;
+
+import java.util.*;
+
+public class Main {
+
+    public static Scanner scan = new Scanner (System.in);
+    public static Random rand = new Random();
+
+    public static void casinoMenu() {
+        System.out.println("Which game would you like to play? \n (1) Blackjack.\n (2) Roulette. \n (3) Slot Machine.");
+        int userGameChoice = scan.nextInt();
+//        switch(userGameChoice){
+//            case 1:
+//
+//        }
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
+
+
+
